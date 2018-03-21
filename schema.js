b16c7d2997460a88726c853cb0399b86f12513aa
marcosocon <oconmarcos97@gmail.com>
2018-03-21 02:01:58 -0300

    added first type for graphql queries

--- schema.js
+++ schema.js
@@ -1,33 +1,42 @@
 const fetch = require('node-fetch');
+const apiKey = 'Kf9MpidEkf6ozUZa5hAGdQ';
 const bluebird = require("bluebird");
 const xmlParser = bluebird.promisify(require('xml2js').parseString);
 const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLInt,
+  GraphQLString,
 } = require('graphql');
 
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: '...',
 
-fetch('https://www.goodreads.com/author/show.xml?id=18541&key=Kf9MpidEkf6ozUZa5hAGdQ')
-  .then((response) => response.text())
-  .then(xmlParser)
-  .then((data) => console.log(data));
-
-const AuthorType = '';
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: xml => xml.GoodreadsResponse.author[0].name[0]
+    }
+  })
+});
 
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'query',
     description: '...',
 
-    fields: () => {
+    fields: () => ({
       author: {
         type: AuthorType,
         args: {
           id: { type: GraphQLInt }
-        }
+        },
+        resolve: (root, args) => fetch(`https://www.goodreads.com/author/show.xml?id=${args.id}&key=${apiKey}`)
+          .then((response) => response.text())
+          .then(xmlParser)
       }
-    }
+    })
   })
 })
